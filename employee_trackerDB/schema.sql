7c3d0ebbae28c7c53500e5ed2175d56e0070edd5
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-18 07:34:19 -0600

    update code

--- employee_trackerDB/schema.sql
+++ employee_trackerDB/schema.sql
@@ -1,24 +1,35 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP DATABASE  IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employees;
+USE employee_db;
 
-CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL
+CREATE TABLE department 
+	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INTEGER
+CREATE TABLE role(
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    primary key (id)
 );
 
 CREATE TABLE employee(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
+	id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id VARCHAR(30),
+    manager_id INT,
+    PRIMARY KEY (id)
 );
+
+INSERT INTO department(name)
+VALUES ("HR"), ("Tech"), ("Admin"), ("Management");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Director", 100000, 1), ("Office Manager", 75000, 2), ("Web Dev", 80000, 3), ("Intern", 45000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Joe", "Blow", "Director", null), ("Rhonda", "Jones", "Office Manager", 1), ("Black", "Rob", "Web Dev", null ), ("Mike", "Tyson", "Intern", null)
